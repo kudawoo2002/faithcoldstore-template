692de7dbaff38bfd8a4b79366b85515faaf099bc
kudawoo2002 <kudawoo2002@gmail.com>
2024-02-19 11:24:47 +0000

    adding products html and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Adding sticky class to the navbar
 const navbarEl = document.querySelector(".navbar");
 
 window.addEventListener("scroll", () => {
@@ -7,3 +8,30 @@ window.addEventListener("scroll", () => {
     navbarEl.classList.remove("sticky");
   }
 });
+
+// End of Navbar
+
+// Products
+const filterNavEl = document.querySelectorAll(".filter-nav-link");
+filterNavEl.forEach((filterLink) =>
+  filterLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    document
+      .querySelector(".filter-nav-link.active")
+      .classList.remove("active");
+    filterLink.classList.add("active");
+
+    const products = document.querySelectorAll(".product");
+    products.forEach((product) => {
+      product.classList.add("hide");
+      if (
+        filterLink.getAttribute("data-type") ===
+          product.getAttribute("data-type") ||
+        filterLink.getAttribute("data-type") === "all"
+      ) {
+        product.classList.remove("hide");
+      }
+    });
+  })
+);
+// End of Product
